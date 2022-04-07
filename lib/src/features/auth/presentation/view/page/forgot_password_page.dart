69157e0550e5b40b4b11f867942ba63f4305514e
WelissonLima <wellissonsacer2000@gmail.com>
2022-04-07 15:00:30 -0300

    Criação da página de redefinição da senha

---
+++ lib/src/features/auth/presentation/view/page/forgot_password_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forgot Password'),
+      ),
+      body: Container(),
+    );
+  }
+}
